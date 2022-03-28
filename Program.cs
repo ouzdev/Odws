5eeca466c7eb0be124a6db4b90536697facc1242
ouzdev <oguzcangencc@hotmail.com>
2022-03-28 15:14:03 +0300

    Swagger implementasyonu yapıldı

--- Program.cs
+++ Program.cs
@@ -1,10 +1,46 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(new WebApplicationOptions{
     EnvironmentName = Environments.Development
 });
-var app = builder.Build();
-if (!app.Environment.IsDevelopment())
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
 {
-    app.UseExceptionHandler("/Error");
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+  {
+    {
+      new OpenApiSecurityScheme
+      {
+        Reference = new OpenApiReference
+        {
+          Type = ReferenceType.SecurityScheme,
+          Id = "Bearer"
+        },
+        Scheme = "oauth2",
+        Name = "Bearer",
+        In = ParameterLocation.Header,
+
+      },
+      new List<string>()
+    }});
+});
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
 }
 app.MapGet("/", () => "Hello World!");
 
