0846acea561e3fd339be73efa05620201ee6f252
ouzdev <oguzcangencc@hotmail.com>
2022-03-28 14:07:40 +0300

    Ortam değişken tanımları yapıldı

--- Program.cs
+++ Program.cs
@@ -5,8 +5,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
 }
 app.MapGet("/", () => "Hello World!");
 
