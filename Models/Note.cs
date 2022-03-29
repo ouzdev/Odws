35bbea1e784139224a14cccc216f5c18aa93a255
ouzdev <oguzcangencc@hotmail.com>
2022-03-29 11:02:02 +0300

    Notlar get metodu kodlandı.

--- Models/Note.cs
+++ Models/Note.cs
@@ -8,6 +8,6 @@ namespace Odws.Models
         public int Id { get; set; }
         public string Name { get; set; } = null!;
         public string Description { get; set; } = null!;
-        public DateOnly Date { get; set; }
+        public DateTime Date { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,19 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using Odws.Models;
 
+#region Create Builder
 var builder = WebApplication.CreateBuilder(new WebApplicationOptions{
     EnvironmentName = Environments.Development
 });
+#endregion
+
+#region Db context
+var connectionString = builder.Configuration.GetConnectionString("PostgreSQLConnection");
+builder.Services.AddDbContext<OdwsDatabaseContext>(x=>x.UseNpgsql(connectionString));
+#endregion
+
+#region Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
@@ -34,14 +45,28 @@ builder.Services.AddSwaggerGen(c =>
       new List<string>()
     }});
 });
+#endregion
+
+#region Builder build
 var app = builder.Build();
+#endregion
+
+#region Environment check
 if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
 }
-app.MapGet("/", () => "Hello World!");
+#endregion
+
+
+app.MapGet("/", () => "Odws Not Uygulamasına Hoşgeldiniz.");
+app.MapGet("/Notes",(OdwsDatabaseContext context)=>{
+
+    var notes =  context.Notes.ToList();
+   return notes;
+});
 
 app.Run();
 
