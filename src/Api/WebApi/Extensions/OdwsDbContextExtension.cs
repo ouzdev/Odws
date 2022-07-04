25d667098eb3a7fd49badfcf4bb95d2282b0a4d0
ouzdev <oguzcangencc@hotmail.com>
2022-07-04 23:34:31 +0300

    ✨ Implement onion architecture

---
+++ src/Api/WebApi/Extensions/OdwsDbContextExtension.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Odws.Models;
+
+public static class OdwsDbContextExtension
+{
+
+    public static IServiceCollection UseOdwsDbContext(this IServiceCollection services, WebApplicationBuilder options)
+    {
+        var connectionString = options.Configuration.GetConnectionString("PostgreSQLConnection");
+        services.AddDbContext<OdwsDatabaseContext>(x => x.UseNpgsql(connectionString));
+        return services;
+    }
+}
+
---
+++ src/Api/WebApi/Models/OdwsDatabaseContext.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Odws.Models
+{
+    public partial class OdwsDatabaseContext : DbContext
+    {
+        public OdwsDatabaseContext()
+        {
+        }
+
+        public OdwsDatabaseContext(DbContextOptions<OdwsDatabaseContext> options)
+            : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Note> Notes { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("Host=localhost;Username=postgres;Password=root;Database=OdwsDb");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Note>(entity =>
+            {
+                entity.ToTable("notes");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Date)
+                    .HasColumnName("date")
+                    .HasDefaultValueSql("CURRENT_DATE");
+
+                entity.Property(e => e.Description)
+                    .HasMaxLength(50)
+                    .HasColumnName("description");
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .HasColumnName("name");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
